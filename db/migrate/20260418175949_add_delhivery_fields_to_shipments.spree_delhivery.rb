364f0238e131efa82263e830b509f052cc55e78a
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-04-18 18:10:32 +0000

    Make Delhivery shipment migrations safe and idempotent

--- db/migrate/20260418175949_add_delhivery_fields_to_shipments.spree_delhivery.rb
+++ db/migrate/20260418175949_add_delhivery_fields_to_shipments.spree_delhivery.rb
@@ -1,11 +1,24 @@
 # This migration comes from spree_delhivery (originally 20250101000001)
 class AddDelhiveryFieldsToShipments < ActiveRecord::Migration[6.1]
-    def change
+  def change
+    unless column_exists?(:spree_shipments, :delhivery_waybill)
       add_column :spree_shipments, :delhivery_waybill, :string
+    end
+
+    unless column_exists?(:spree_shipments, :delhivery_ref_id)
       add_column :spree_shipments, :delhivery_ref_id, :string
+    end
+
+    unless column_exists?(:spree_shipments, :delhivery_label_url)
       add_column :spree_shipments, :delhivery_label_url, :text
+    end
+
+    unless column_exists?(:spree_shipments, :delhivery_response_data)
       add_column :spree_shipments, :delhivery_response_data, :json # Store full debug response
-      
+    end
+    
+    unless index_exists?(:spree_shipments, :delhivery_waybill)
       add_index :spree_shipments, :delhivery_waybill
     end
   end
+end
