1d8ea0b90548bba7c097a3cebd0aa366a198280c
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-06-25 06:38:40 +0530

    Update password_reset_subscriber.rb

--- app/subscribers/spree/password_reset_subscriber.rb
+++ app/subscribers/spree/password_reset_subscriber.rb
@@ -1,18 +1,16 @@
 module Spree
   class PasswordResetSubscriber < Spree::Subscriber
     subscribes_to 'customer.password_reset_requested'
-
+ 
     def handle(event)
       email = event.payload['email']
       token = event.payload['reset_token']
-      store_id = event.store_id
-
+ 
       user = Spree.user_class.find_by(email: email)
       return unless user
-
-      Spree::UserMailer.reset_password_instructions(
-        user, token, { current_store_id: store_id }
-      ).deliver_later
+ 
+      # Spree 5 removed Spree::UserMailer. Use Devise::Mailer directly.
+      Devise::Mailer.reset_password_instructions(user, token).deliver_later
     end
   end
 end
