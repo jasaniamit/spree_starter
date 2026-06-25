1e45d5b6a92f5907b5d161a3b5a38ad55904bad6
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-06-25 15:28:59 +0530

    Delete app/models/spree/user.rb

--- app/models/spree/user.rb
+++
@@ -1,7 +0,0 @@
-# Spree::User is fully defined by spree_auth_devise gem.
-# We defer any customisations until after all gems are loaded.
-Rails.application.config.to_prepare do
-  Spree::User.class_eval do
-    # Add custom methods, associations, or validations here if needed.
-  end
-end
