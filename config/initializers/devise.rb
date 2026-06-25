9e42ea354ca656674cf69fc3f931260222cf6ca0
jasaniamit <67775072+jasaniamit@users.noreply.github.com>
2026-06-25 07:01:20 +0530

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,8 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   config.secret_key = Rails.application.credentials.secret_key_base || ENV['SECRET_KEY_BASE']
-
+  config.router_name = :main_app
+  
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   config.parent_controller = 'Spree::BaseController'
